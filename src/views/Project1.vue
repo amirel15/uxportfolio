a10d9c31fc426c2e413c8082ef5365f4cbfa48e0
ashle <amireles1334@yahoo.com>
2023-05-09 14:03:16 -0500

    project 1 configured

--- src/views/Project1.vue
+++ src/views/Project1.vue
@@ -1,7 +1,157 @@
 <template>
-    <h1 class="text-6xl font-thin text-slate-100">Project 1<br/>Mobile Hospital Application</h1>
-    <div class="parts">
-        <a href="https://www.figma.com/proto/Y6mbGJNuo8DbtlaYAbaGEV/Hospital-Staff-Scheduling-App?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2">Low-Fidelity Prototype<br/></a>
-        <a href="https://www.figma.com/proto/l8V6cRlk60L8a3G67cxilV/Hospital-App-New?type=design&node-id=1-5&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A5">High-Fidelity Prototype<br/></a>
+    <div class="wrapper">
+    <div class="title">
+        Project 1:<br/>Mobile Hospital Application
     </div>
+    <div class="overview">
+        <div class="ptitle">Project Overview</div>
+        <div class="pbody">
+            <b>The Product:</b> MetroU Hospital is an institution that has been around since the early 2000s. It is the go to place in the
+            metropolitan area for all your medical needs from routine to emergency.
+            <br/><b>Project Duration:</b> January 2023 - May 2023
+            <br/><b>The Problem:</b> Hospital employees have to use an internet browser on a desktop or go in person to find
+            out work schedule or find other employee information.
+            <br/><b>The Goal:</b> Develop a mobile applicaiton that will solve the mentioned problem all in the palm of your hand.
+            <br/><b>My Role:</b> UX Designer
+            <br/><b>Responsibilities:</b> user research, wireframing, prototyping<br/>
+        </div>
+    </div>
+    <div class="users">
+        <div class="utitle">Understanding the User</div>
+        <div class="ubody">
+            <b>Summary</b>
+            <br/>I conducted research by use of questionnaires and surveys amongst employees throughout the hospital to find out how they
+            felt about always having to call the hospital or come in on their day off to find out about the schedule. I noticed one of the
+            main departments that had issues was the pediatric surgical department. Based off what was discovered in the research findings,
+            individuals who participated in multiple departments sometimes had trouble finding information about if they had coverage or staffing.
+            <br/><br/><b>Pain Points</b>
+            <br/><b>Time:</b>  Working adults dont always have time to stop what they are doing to find a desktop to view what they need.
+            <br/><b>Accessibility:</b>  Not everyone always has a desktop in front of them 24/7. There are only limited ways at the moment to
+            access needed information.
+            <br/><b>Information Being in Multiple Places:</b>  Schedule is in one place while the contact information of the staff is in other.
+            Adds more duties one has to do than needed.
+        </div>
+        </div>
+
+        <div class="persons">
+            <div class="titleuser">User Personas</div>
+        <div class="upersonas1">
+            <b>Persona 1</b>
+            <br/><b>Name:</b> Jeffery
+            <br/><i>“I am all about helping others. In order to help them, I must first take care of myself. I love the quiet I find in nature and use it as a way to
+            decompress after a long week.”</i>
+            <br/><br/><b>About</b>
+            <br/>He is a 25 year old male living in Austin, TX. He currently has a Bachelor's degree in Nursing and works as an ICU Nurse.
+            <br/>Jeffery is an ICU nurse with a very busy and hectic schedule. They work in the Cardiac ICU of a major metropolitan hospital. On their free time, they enjoy
+            hiking and exploring nature. Since they work in a very busy hospital, ensuring there is always coverage is important. Jeffery would like an easier to navigate
+            scheduling application to ensure the rest of his team is playing to their strengths, especially at peak times.
+            <br/><br/><b>Goals:</b>
+            <br/>-To be what their patients need when they need it
+            <br/>-To balance a healthy relationship between work & personal life
+            <br/>-To find a smoother transition when it comes to shift changes to benefit both employees & patients
+            <br/><br/><b>Frustrations:</b>
+            <br/>“Being short staffed on the floor due to scheduling errors makes more unnecessary work.”
+            <br/>“If the scheduling app allowed for me to see Cardiac Departments shifts, I could make a more efficient schedule.”
+        </div>
+        <div class="upersonas2">
+            <br/><br/><b>Persona2</b>
+            <br/><b>Name:</b> Alex
+            <br/><i>“Ever since having a child, I have learned how precious time can be. Whenever I am not at work, I try to make
+            the best of it and make memories with my little one.”</i>
+            <br/><br/><b>About</b>
+            <br/>He is a 38 year old male from Nashville, TN. He currently has a PhD and MD degree and works as a Pediartic Suregon.
+            <br/>Alex has been a pediatric surgeon for about a decade. They used to work at a small hospital that didnt handle as many
+            patients as their current hospital does. They are still getting used to the demand and how often scheduling can be affected
+            due to an emergency. They dont let this bother them. They are just trying to make it work to where they can still get home
+            in time to tuck in their child whenever possible.
+            <br/><br/><b>Goals:</b>
+            <br/>-To make every patient feel a little better after surgery
+            <br/>-To try & do the minimally invasive surgery possible to make the childs recovery quicker & smoother
+            <br/>-To try to make it home at a decent hour when not on call to spend time with family instead of in an OR
+            <br/><br/><b>Frustrations:</b>
+            <br/>“Sometimes staff scheduling issues in other departments cause me to have to postpone or delay my patient's surgeries.”
+            <br/>“Sometimes I wish I could be made known somehow that my scheduled non emergent surgeries had the staff needed scheduled.”
+        </div>
+    </div>
+
+    <div class="prototypes">
+        <div class="prtitle">Usability Study: Findings</div>
+        <div class="prfind">            
+            <br/>I conducted a few studies. Findings from the first study helped me decide to shrink down the population field before expanding
+            to more areas in the hospital. This is because different departments have different responsibilities and areas needed for access.
+            Individuals in the second study used a high fidelity prototype and recommended adding more color and removing repeated words.
+            <br/><br/><b>Round 1 Findings</b>
+            <br/>-Users want an app relevant to their department
+            <br/>-Users wanted a menu option instead of having to go back to home page every time
+            <br/><br/><b>Round 2 Findings</b>
+            <br/>-Users wanted more color to help differentiate objects on screen
+            <br/>-Users felt some words on screen were repetitive which caused confusion
+            <br/><br/><b>Accessibility Considerations</b>
+            <br/>-Added extra screens to eliminate clutter
+            <br/>-Added color to make it easier to differentiate objects or icons on screen
+            <br/>-Tried to add as little as needed of necessary text to avoid confusion and clutter
+            <br/><br/><b>Prototypes</b><br/>
+        <a href="https://www.figma.com/proto/Y6mbGJNuo8DbtlaYAbaGEV/Hospital-Staff-Scheduling-App?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2">Click for Low-Fidelity Prototype<br/></a>
+        <a href="https://www.figma.com/proto/l8V6cRlk60L8a3G67cxilV/Hospital-App-New?type=design&node-id=1-5&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A5">Click for High-Fidelity Prototype<br/></a>
+        </div>
+    </div>
+
+    <div class="conclusion">
+        <div class="ctitle">Key Takeaways & Next Steps</div>
+        <div class="takeaways">            
+            <br/><b>Impact</b>
+            <br/>I implemented this design towards another department in the hospital before combining the two and slow expand until
+            as much of the hospital departments and employees are included and have access.
+            <br/><br/><b>What I Learned</b>
+            <br/>I learned to not start off too big at the beginning. W h ile the goal for this application to for a whole
+            institution, starting small with just one department helped me better listen to the needs of the users and make the process easier in the future.
+        </div>
+        <div class="next">
+            <br/><b>Next Steps</b>
+            <br/>-Conduct one more study with the necessary changes made and implemented before expanding applicaiton larger.
+            <br/>-Repeat same process with the next department.
+            <br/>-Hopefully be able to combine the two departments with one functional application.
+        </div>
+    </div>
+    </div>  
+
 </template>
+
+<style scoped lang="postcss">
+.wrapper {
+    @apply flex flex-col container mx-auto py-5 gap-4;
+}
+.title {
+        @apply rounded-md bg-blue-200 p-10 text-slate-900 text-center italic font-bold text-3xl;
+    }
+.overview{
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .ptitle {
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.users {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .utitle {
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.persons {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .titleuser{
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.prototypes {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+    .prtitle{
+        @apply text-center text-2xl font-bold underline;
+    }
+}
+.conclusion {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .ctitle{
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+</style>
