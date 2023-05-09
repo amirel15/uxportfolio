b19196918efdf43a37b65d558220d8c45638b4a6
ashle <amireles1334@yahoo.com>
2023-05-09 14:38:04 -0500

    project 3 info added

--- src/views/Project3.vue
+++ src/views/Project3.vue
@@ -1,7 +1,160 @@
 <template>
-    <h1 class="text-6xl font-thin text-slate-100">Project 3<br/>Lets Make Kids Meals Web Application</h1>
-    <div class="parts">
-        <a href="https://www.figma.com/proto/yVF1h1JIAtRZD88EJyrqJk/Lets-Make-Kids-Meals-lofi?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A26">Low-Fidelity Prototype<br/></a>
-        <a href="https://www.figma.com/proto/TWJBHuGDXQogNmww96Pmol/Lets-Make-Kids-Meals-HiFi?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2">High-Fidelity Prototype</a>
+    <div class="wrapper">
+    <div class="title">
+        Project 3:<br/>Lets Make Kids Meals Web Application
     </div>
+    <div class="overview">
+        <div class="ptitle">Project Overview</div>
+        <div class="pbody">
+            <br/><b>The Product:</b> The Lets Make Kids Meals application is a product of KidsUp Co. This company focuses on trying to
+            make kids more able-bodied to real world experiences. KidsUp Co. offers programs, applications, and utilities that help
+            a child become more independent and ready them for when they are older.            
+            <br/><b>Project Duration:</b> January 2023 - May 2023
+            <br/><b>The Problem:</b> Growing up, not all perople are exposed to skills and experiences that they will need when they
+            are older. Some individuals dont start learning how to cook until they are in their 20s, and that is because they are forced to.
+            <br/><b>The Goal:</b>  This application helps kids, from their young years to their teen years, learn how to make recipes in
+            their very own kitchen. It offers meals from breakfast and dinner to snacks.
+            <br/><b>My Role:</b> UX Designer, UX Researcher
+            <br/><b>Responsibilities:</b> develop a kid-friendly app that kids of all ages can maneuver, execute surveys and trial runs with
+            potential users to better the application<br/>
+        </div>
+    </div>
+    <div class="users">
+        <div class="utitle">Understanding the User</div>
+        <div class="ubody">
+            <b>Summary</b>
+            <br/>We conducted research by taking surveys and questionaries from kids who ages 5 to 18. We asked them a series of questions to see
+            how much they knew about cooking, how experienced they were, how they learned how to cook, and how do they feel about recipe apps.
+            The older individuals who were surveyed seem to all a similar consensus that they wished more recipe apps had simpler recipes to work
+            with. They had the desire to want to learn how to cook for themselves or their families, but some recipes felt “too complex or challenging”.
+            The individuals, from various age groups, that did not have phone access yet did have access to a tablet or desktop. This provided insight
+            for us to develop a mobile app more configured for tablet screens than phone screens because it allowed for more space, less clutter, and
+            to include more youngsters to use the application.
+        </div>
+        </div>
+
+        <div class="persons">
+            <div class="titleuser">User Personas</div>
+        <div class="upersonas1">
+            <b>Persona 1</b>
+            <br/><b>Name:</b> Julianna
+            <br/><i>“I love mealtime, especially dinner, because no matter how busy our day is, we always sit down for at least one meal a day as a family.”</i>
+            <br/><br/><b>About</b>
+            <br/>She is a 14 year old female high school freshman living in McAllen. She lives with her parents and two younger siblings.
+            <br/>Julianna comes from a loving family where they all pitch in and help each other where they can. She is the oldest of the 3 kids in her family.
+            Both her parents work full-time. While they have quality family time on the weekends, she sees how tired her parents can be during the week and wants
+            to help out by making dinner once or twice to let them relax a little. She knows it might not be much be even just cooking for the family once a week
+            would mean a lot to her parents. She wants to make them proud, and she knows that learning to cook will help her a lot as she gets older.
+            <br/><br/><b>Goals:</b>
+            <br/>“I want to learn how to cook family sized meals, so I can help make a meal from time to time.”
+            <br/>“Dinner and desserts are my favorite. I need to learn some yummy recipes for them to share with my siblings."
+            <br/><br/><b>Frustrations:</b>
+            <br/>“I dont have much experience in the kitchen cooking on my own. Ive helped my parents from time to time but have never made a family sized meal all
+            on my own. I dont want to mess it up.”
+
+        
+        </div>
+        <div class="upersonas2">
+            <br/><br/><b>Persona2</b>
+            <br/><b>Name:</b> Roman
+            <br/><i>“Im a big kid now. I want to be the cook. Lets cook something yummy.”</i>
+            <br/><br/><b>About</b>
+            <br/>He is a 6 year old boy in kinder. He lives in San Antonio, TX with his parents and little brother.
+            <br/>Roman is a growing young 6-year-old with a massive imagination and will to learn. As he continues to grow, he wants to be more involved with what
+            his parents do. He wants his next “big boy” challenge to be learning to cook. He loves to eat food but wants to know how its made. He needs recipes that
+            are kid-friendly, even for those who might not be the best readers.
+            <br/><br/><b>Goals:</b>
+            <br/>“I need to help mom and dad make food for little brother.”
+            <br/>“I want to be able to do something more than just put the plates on the table.”
+            <br/><br/><b>Frustrations:</b>
+            <br/>“I dont read very well, so I have trouble reading recipes. I wish they had more pictures.”
+
+        </div>
+    </div>
+
+    <div class="prototypes">
+        <div class="prtitle">Usability Study</div>
+        <div class="prfind">            
+            <br/><b>Study Type:</b> Unmoderated usability study
+            <br/><b>Location:</b> Texas, USA, Remote
+            <br/><b>Participants:</b> 15 participants ages 5 to 18 (those who need assistance received it from their parents)
+            <br/><b>Length:</b> 15 minutes to 1 hour (depending on their reading ability)
+            <br/><br/><b>Findings</b>
+            <br/>-It was very detailed, so those able to read were able to understand the recipe well. However, those without strong reading skills felt discouraged
+            to want to attempt recipe.
+            <br/>-Participants or parents of participants that needed assistance felt that a disclosure should be added somewhereas a precaution because knives and
+            the stove/oven are used.
+            <br/>-Younger participants voiced they would like pictures added to help identiy ingredients so they could be of more assistance when making the recipe.
+            <br/><br/><b>Accessibility Considerations</b>
+            <br/>-Add pictures of ingredients to help better identify what ingredients are needed for the recipe.
+            <br/>-Separated the ingredients and instructions of each recipe into two different pages to declutter application.
+            <br/><br/><b>Prototypes</b><br/>
+        <a href="https://www.figma.com/proto/yVF1h1JIAtRZD88EJyrqJk/Lets-Make-Kids-Meals-lofi?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A26">Click for Low-Fidelity Prototype<br/></a>
+        <a href="https://www.figma.com/proto/TWJBHuGDXQogNmww96Pmol/Lets-Make-Kids-Meals-HiFi?type=design&node-id=1-2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2">Click for High-Fidelity Prototype</a>
+        </div>
+    </div>
+
+    <div class="conclusion">
+        <div class="ctitle">Key Takeaways & Next Steps</div>
+        <div class="takeaways">            
+            <br/><b>Impact</b>
+            <br/>After following recommendations from out study participants, we were able to redesign and make an application that allows for youngsters of all ages
+            to be able to navigate and helps them further their desires in the kitchen.
+
+            <br/><br/><b>What I Learned</b>
+            <br/>I learned is listen to your users. They’ll be the ones making most use of the application and you want them coming back for more. They helped me
+            realize to start off small and to be able to listen to each of their concerns and recommendations to get a stable simple application before expanding to
+            something larger.
+
+        </div>
+        <div class="next">
+            <br/><b>Next Steps</b>
+            <br/>-Add more recipes for each mealtime.
+            <br/>-Maybe consider breaking down each mealtime a little more once more recipes are added to make it easier for users to decide which recipe they want to
+            try based on difficulty and complexity.
+            <br/>-Maybe add a page that allows users that aren’t part of the initial studies to make recommendations to better the application or for adding certain
+            recipes they have interest in.
+        </div>
+    </div>
+    </div>  
+
 </template>
+
+<style scoped lang="postcss">
+.wrapper {
+    @apply flex flex-col container mx-auto py-5 gap-4;
+}
+.title {
+        @apply rounded-md bg-blue-200 p-10 text-slate-900 text-center italic font-bold text-3xl;
+    }
+.overview{
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .ptitle {
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.users {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .utitle {
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.persons {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .titleuser{
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+.prototypes {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+    .prtitle{
+        @apply text-center text-2xl font-bold underline;
+    }
+}
+.conclusion {
+     @apply rounded-md bg-blue-200 py-5 px-3 text-slate-900 italic font-bold text-xl;
+     .ctitle{
+        @apply text-center text-2xl font-bold underline;
+     }
+}
+</style>
